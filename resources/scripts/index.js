16033fea425e0d8b8bbf19eac322c0b87220a9c6
Andres Borrelli <andresborrelli@gmail.com>
2025-07-26 13:00:35 -0300

    Avance 2025-07-24 menu 2

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,6 +1,17 @@
 //------------------------------------------------------------------------------
-document.addEventListener('DOMContentLoaded', () => {
 
-    
-}
+//------------------------------------------------------------------------------
+window.addEventListener('load', (e) => {
+  
+    const navItems = document.querySelectorAll('.main-nav-item');
+  
+    for (var i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener('click', e => {
+            var mainNavButton = document.getElementById('main-nav-button');
+            mainNavButton.checked = false;
+        });
+
+    }
+
+});
 //------------------------------------------------------------------------------
